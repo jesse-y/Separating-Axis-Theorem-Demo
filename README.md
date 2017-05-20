b45902d4686e41df41059d414438170104468630
jesse-y <jesse.yue@gmail.com>
2017-05-20 12:53:21 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Separating-Axis-Theorem-Demo
 An interactive collision detection demo using the separating axis theorem.
+
+This demo uses a canvas object, and will require a browser with HTML5 support.
---
+++ js/input.js
@@ -0,0 +1,90 @@
+(function () {
+	var keys = {};
+	var mouse = {
+		x: 0,
+		y:0,
+		clicked:false
+	};
+
+	function setKey(event, status) {
+		var code = event.keyCode;
+		var key;
+
+		switch(code) {
+			case 38:
+			case 87:
+				key = 'UP'; break;
+			case 40:
+			case 83:
+				key = 'DOWN'; break;
+			case 37:
+			case 65:
+				key = 'LEFT'; break;
+			case 39:
+			case 68:
+				key = 'RIGHT'; break;
+			case 27:
+				key = 'ESCAPE'; break;
+			default:
+				key = String.fromCharCode(code);
+		}
+		keys[key] = status;
+	}
+
+	document.addEventListener('keydown', function(e) {
+		setKey(e, true);
+	})
+	document.addEventListener('keyup', function(e) {
+		setKey(e, false);
+	})
+	document.addEventListener('blur', function () {
+		keys= {};
+	})
+	document.addEventListener('mousemove', function(e) {
+		mouse.x = e.clientX;
+		mouse.y = e.clientY;
+	})
+	document.addEventListener('mousedown', function(e) {
+		mouse.clicked = true;
+	})
+	document.addEventListener('mouseup', function(e) {
+		mouse.clicked = false;
+	})
+
+	window.input = {
+		is_down: function(key) {
+			return keys[key.toUpperCase()];
+		},
+		all_keys: function() {
+			return keys;
+		},
+		mouseX: function() {
+			return mouse.x;
+		},
+		mouseY: function() {
+			return mouse.y;
+		},
+		mouse_clicked: function() {
+			return mouse.clicked;
+		},
+		get_commands: function() {
+			var commands = [];
+			if (keys['UP']) {
+				commands.push('UP');
+			}
+			if (keys['DOWN']) {
+				commands.push('DOWN');
+			}
+			if (keys['LEFT']) {
+				commands.push('LEFT');
+			}
+			if (keys['RIGHT']) {
+				commands.push('RIGHT');
+			}
+			if (keys['ESCAPE']) {
+				commands.push('ESCAPE');
+			}
+			return commands;
+		}
+	};
+})();
